feb69ea4be7f3feb4b2c8c5a9447ec687df2975f
Pedro henrique Correa teixeira <studionoronha@yahoo.com>
2019-10-11 10:20:32 -0400

    Temperatura em Fah

---
+++ temp.js
@@ -0,0 +1,7 @@
+const kelvin=293;
+// Variavel constante da temperatura
+const celsius=273 - kelvin;
+// A temperatura do grau Celsius é 273 subtraindo o kelvin
+var fahrenheit= Math.floor(celsius*(9/5)+32);
+// formula Fah
+console.log(` A temperatura é ${fahrenheit} graus em Fahrenheit`);
